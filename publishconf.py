f2a112cee0d7b0255f77aff62e0421eb200e6822
sornars <sornars@users.noreply.github.com>
2014-11-23 00:10:52 +0000

    Changed URLs from bare domain to www to prevent unnecessary redirects.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://sornars.com'
+SITEURL = 'http://www.sornars.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
